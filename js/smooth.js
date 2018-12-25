a4e1fe66e744c4817e9c37e3e4fcdf9a1c71733b
jehu12 <43791726+jehu12@users.noreply.github.com>
2018-12-25 16:09:08 +0800

    Update smooth.js

--- js/smooth.js
+++ js/smooth.js
@@ -19,16 +19,16 @@ window.sr = ScrollReveal();
 //   duration: 2000,
 //   origin:'bottom'
 // });
-// sr.reveal('.showcase-left', {
-//   duration: 2000,
-//   origin:'top',
-//   distance:'300px'
-// });
-// sr.reveal('.showcase-right', {
-//   duration: 2000,
-//   origin:'right',
-//   distance:'300px'
-// });
+ sr.reveal('.showcase-left', {
+   duration: 2000,
+   origin:'top',
+   distance:'300px'
+ });
+ sr.reveal('.showcase-right', {
+   duration: 2000,
+   origin:'right',
+   distance:'300px'
+ });
 // sr.reveal('.showcase-btn', {
 //   duration: 2000,
 //   delay: 2000,
